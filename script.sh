9cbc667f87f030ad673b5ccfebf3381833643ef7
vxue <vxue@node004.cm.cluster>
2018-07-11 22:53:48 -0400

    rename dir

---
+++ script.sh
@@ -0,0 +1,14 @@
+
+for each in `cat dirs`;
+	do 
+	cd $each/workspace/;
+	for bar in barcode*;
+		do 
+		echo $each/$bar /scratch/users/vxue/data/vxue@mit.edu/$each'_'$bar'_'0;
+	done;
+	cd ../../;
+done;
+
+
+#sed -ne '1~8p;2~8p;3~8p;4~8p' x.fastq > x_1.fastq
+#sed -ne '5~8p;6~8p;7~8p;8~8p' x.fastq > x_2.fastq
