57840bb2273346c745c0ff7a7a39e64f652aefb7
vxue <vxue@login002.cm.cluster>
2018-07-14 18:30:19 -0400

    script to split the fastq files

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ for each in `cat dirs`;
 	cd $each/workspace/;
 	for bar in barcode*;
 		do 
-		echo $each/$bar /scratch/users/vxue/data/vxue@mit.edu/$each'_'$bar'_'0;
+		sed -ne '1~8p;2~8p;3~8p;4~8p' $bar > /scratch/users/vxue/data/vxue_mit_edu/$each'_'$bar'_'0;
+		sed -ne '5~8p;6~8p;7~8p;8~8p' $bar > /scratch/users/vxue/data/vxue_mit_edu/$each'_'$bar'_'1
 	done;
 	cd ../../;
 done;
